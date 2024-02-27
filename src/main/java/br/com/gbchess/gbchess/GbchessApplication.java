a61168a67b25fc180e541318315be7f4f5beee10
Gabriel Arraes <gabriel.arraes@sicoob.com.br>
2024-02-27 15:59:13 -0300

    init project

---
+++ src/main/java/br/com/gbchess/gbchess/GbchessApplication.java
@@ -0,0 +1,13 @@
+package br.com.gbchess.gbchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbchessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gbchess/gbchess/GbchessApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gbchess.gbchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
